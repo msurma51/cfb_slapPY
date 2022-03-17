380f833b84ceafe324098177738ca0e181e52a7d
Mark Surma <surmam511@gmail.com>
2022-03-17 08:27:05 -0400

    Update with sql init

---
+++ slapPY_top.py
@@ -0,0 +1,10 @@
+from game_spider import box_score_links
+from pbp_parser_func import *
+from play_maker import *
+games = list()
+for url in box_score_links:
+    quarters,name_dict = pbp_parser(url)
+    game_info = game_builder(quarters,name_dict)
+    games.append(game_info)
+    state = game_info[1]
+    print(state['away_abbr'],'@',state['home_abbr'],'parsed successfully')
