9a5f56d645b030edbe348fb2f2430119a8cad442
Mark Surma <surmam511@gmail.com>
2023-08-16 06:49:01 -0400

    playmaker_new update

--- src/play_maker_funcs.py
+++ src/play_maker_funcs.py
@@ -6,8 +6,9 @@ Created on Mon Jul  3 18:21:16 2023
 """
 
 import pandas as pd
+import numpy as np
 
-def name_extract(df,start_pattern,end_pattern,*name_patterns, prefix = ''):
+def name_extract(str_ser,start_pattern,end_pattern, name_patterns, prefix = '', flags = 0):
     '''
     
 
@@ -23,10 +24,22 @@ def name_extract(df,start_pattern,end_pattern,*name_patterns, prefix = ''):
 
     '''
     patterns = [start_pattern + name_pattern + end_pattern for name_pattern in name_patterns]
-    df_phase = df['play_str'].str.extract(patterns[0]).fillna('')
+    df_phase = str_ser.str.extract(patterns[0], flags = flags).fillna('')
     if len(patterns) > 1:
         for pattern in patterns[1:]:
-            df_new = df['play_str'].str.extract(pattern).fillna('')
+            df_new = str_ser.str.extract(pattern, flags = flags).fillna('')
             for col in df_phase.columns:
                 df_phase[col] = df_phase[col] + df_new[col]
     return df_phase.rename(columns = {name: prefix + name for name in ('first','last')})
+
+def kick_extract(str_ser, kick_type, name_patterns):
+    df_kicker = name_extract(str_ser, '^', f" {kick_type}", name_patterns, prefix ='kicker_')
+    df_kicker['kick_type'] = np.where(df_kicker['kicker_first'].str.len() > 0, kick_type, '')
+    df_yards = str_ser.str.extract(kick_type + " (?P<kick_yards>\d{1,2}) yard(?:s)?")
+    yl_str = ".*? to the (?P<kick_terr>[A-Z\d']+?)(?P<kick_yl>[0-9]{1,2})"
+    df_yl = str_ser.str.extract(kick_type + yl_str)
+    return pd.concat((df_kicker, df_yards, df_yl), axis = 1)
+
+def regex_check(df):
+    df_check = df.replace('', np.NaN)
+    return df_check.dropna(axis = 0, how = 'all')
--- src/play_maker_new.py
+++ src/play_maker_new.py
@@ -6,8 +6,9 @@ Created on Sat Feb 11 15:46:33 2023
 """
 import pandas as pd
 import numpy as np
+import re
 from presto_prep import headers, pot, presto_parser
-from play_maker_funcs import name_extract
+from play_maker_funcs import name_extract, kick_extract, regex_check
 from bs4 import SoupStrainer
 
 
@@ -63,7 +64,7 @@ direction_pat1 = " left| middle| right"
 direction_pat2 = " over left end| up middle| over right end"
 rush_dir_pat = "(?P<run_dir1>{})?".format(direction_pat1) + " rush" + "(?P<run_dir2>{})?".format(direction_pat2) 
 # "Overlay" dataframes using concatenation and add columns to df
-df_rusher = name_extract(df,"^",rush_dir_pat,*name_patterns, prefix = 'rusher_')
+df_rusher = name_extract(df['play_str'],"^",rush_dir_pat, name_patterns, prefix = 'rusher_')
 # Get gain/loss and yardline rushed to info
 rush_pat2 = " rush" + "(?:{})?".format(direction_pat2)
 run_gl_pat = " for (?P<run_gain>(?:loss of )?\d{1,2}) yards?"
@@ -80,27 +81,83 @@ df = pd.concat((df,df_rusher,df_gains),axis = 1)
 # Extract pass play info
 # Get passer, direction and result for passes thrown
 pass_dir_pat = "(?P<pass_dir> LE| middle| RE)? pass (?P<pass_result>(?:in)?complete)"
-df_passer = name_extract(df,"^",pass_dir_pat,*name_patterns, prefix = 'passer_')
+df_passer = name_extract(df['play_str'],"^",pass_dir_pat,name_patterns, prefix = 'passer_')
 # Get passer and result for sacks
-df_sacked = name_extract(df,"^"," (?P<pass_result>sack)ed",*name_patterns, prefix = 'passer_')
+df_sacked = name_extract(df['play_str'],"^"," (?P<pass_result>sack)ed", name_patterns, prefix = 'passer_')
 # Overlay df_sacked into df_passer
 for col in df_sacked.columns:
     df_passer[col] = df_passer[col] + df_sacked[col]
 # Get intended receiver info
-df_intended = name_extract(df," pass (?:in)?complete to ",'',*name_patterns, prefix = 'intended_')
+df_intended = name_extract(df['play_str']," pass (?:in)?complete to ",'', name_patterns, prefix = 'intended_')
 # If pass is intercepted, get interceptor info
 int_start_pat = " pass (?P<pass_result>intercepted) by "
 int_end_pat = " at the (?P<int_terr>[A-Z\d']+?)(?P<int_yl>[0-9]{1,2})"
-df_intercepted = name_extract(df,int_start_pat,int_end_pat,*name_patterns, prefix = 'intBy')
+df_intercepted = name_extract(df['play_str'],int_start_pat,int_end_pat, name_patterns, prefix = 'intBy_')
 # Overlay inteception result into df_passer and drop column from df_intercepted
 df_passer['pass_result'] = df_passer['pass_result'] + df_intercepted['pass_result']
 df_intercepted.drop('pass_result', axis=1, inplace=True)
 # Get player info for hurries and pass break-ups
-df_brokenUpBy = name_extract(df,' broken up by','',*name_patterns,prefix = 'pbuBy')
-df_hurriedBy = name_extract(df,' QB hurried by','',*name_patterns,prefix = 'hurriedBy')
+df_brokenUpBy = name_extract(df['play_str'],' broken up by','', name_patterns,prefix = 'pbuBy_')
+df_hurriedBy = name_extract(df['play_str'],' QB hurried by','', name_patterns,prefix = 'hurriedBy_')
+# Get gain/loss info for passes and sacks
 pass_pats = [pattern.replace('run_','pass_') for pattern in (run_gl_pat, run_ng_pat, run_to_pat)]
+pass_start_pat = " pass (?:in)?complete to.*?"
+df_pass_gains = df['play_str'].str.extract(pass_start_pat + pass_pats[0] + pass_pats[2]).fillna('')
+df_pass_noGain = df['play_str'].str.extract(pass_start_pat + pass_pats[1] + pass_pats[2]).fillna('')
+df_sack_loss = df['play_str'].str.extract('sacked.*?' + pass_pats[0] + pass_pats[2]).fillna('')
+for col in df_pass_gains.columns:
+    df_pass_gains[col] = df_pass_gains[col] + df_pass_noGain[col] + df_sack_loss[col]
+# Combine with main dataframe
+df = pd.concat((df, df_passer, df_intended, df_intercepted, df_brokenUpBy, df_hurriedBy,
+                df_pass_gains), axis = 1)
 
+# Extract extra point info
+df_xp = pd.DataFrame()
+for xp_type in ('kick', 'rush', 'pass'):
+    xp_result_pat = f' {xp_type} attempt (?P<xp_result>good|successful|no good|failed)'
+    df_xp_type = name_extract(df['play_str'], '^', f' {xp_type} attempt', name_patterns, prefix = 'xp_')
+    df_xp_type['xp_type'] = np.where(df_xp_type['xp_first'].str.len() > 0, xp_type, '')
+    df_xp_result = df['play_str'].str.extract(xp_result_pat, flags = re.IGNORECASE).fillna('')
+    df_xp_type['xp_result'] = df_xp_result['xp_result']
+    if len(df_xp) == 0:    
+        df_xp = pd.concat((df_xp, df_xp_type), axis = 1)
+    else:
+        for col in df_xp:
+            df_xp[col] = df_xp[col] + df_xp_type[col]
 
+# Extract kickoff and punt info
+df_ko = kick_extract(df['play_str'], 'kickoff', name_patterns)
+df_punt = kick_extract(df['play_str'], 'punt', name_patterns)
 
+# Extract return info
+df_return = name_extract(df['play_str'], '', ' return', name_patterns, prefix = 'ret_')
+df_retYards = df['play_str'].str.extract("return (?P<ret_yards>\d{1,2}) yard(?:s)?")
+yl_str = "return .*? to the (?P<ret_terr>[A-Z\d']+?)(?P<ret_yl>[0-9]{1,2})"
+df_retYL = df['play_str'].str.extract(yl_str)
+df_return = pd.concat((df_return, df_retYards, df_retYL), axis = 1)
+df = pd.concat((df, df_xp, df_ko, df_punt, df_return), axis = 1)
 
+# Extract tackler info
+df['tackle_str'] = df['play_str'].str.extract('(.*)(?:PENALTY.*)$')
+df['tackle_str'] = np.where(df['tackle_str'].isna(), df['play_str'], df['tackle_str'])
+tackler_strings = df['tackle_str'].str.extract("\((.*)\)").squeeze().str.strip().fillna('')
+tackler_df = tackler_strings.str.split(pat = ';', expand = True).fillna('')
+df_tackler1 = name_extract(tackler_df[0], '', '', name_patterns, prefix = 'tackler1_')
+df_tackler2 = name_extract(tackler_df[1], '', '', name_patterns, prefix = 'tackler2_')
+
+# Extract penalty info
+df['pen_str'] = df['play_str'].str.extract('(PENALTY.*)', flags = re.IGNORECASE)
+df['presnap_pen'] = df['play_str'].str.startswith('PENALTY')
+player_pen_strings = df['pen_str'].str.extract("\((.*)\)").squeeze().str.strip().fillna('')
+df_pen = name_extract(player_pen_strings, '', '', name_patterns, prefix = 'pen_')
+pen_pat = "PENALTY (?P<pen_team>[A-Z'\d]+) (?P<penalty>[a-zA-Z ]+[a-zA-Z])? ?(?P<pen_yards>\d{1,2})"
+df_pen = pd.concat((df_pen,df['pen_str'].str.extract(pen_pat)), axis = 1)
+df = pd.concat((df, df_tackler1, df_tackler2, df_pen), axis = 1)
+
+# Set boolean values for certain events
+for tag in ['TOUCHDOWN', 'NO PLAY']:
+    colname = '_'.join(tag.lower().split())
+    df[colname] = np.where(df['play_str'].str.contains(tag, flags = re.IGNORECASE), 1, 0)
+
+regex_check(df['play_str'].str.contains('TOUCHDOWN'))
 
